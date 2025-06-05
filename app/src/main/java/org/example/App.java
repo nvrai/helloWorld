554254702fd70dd0551497552d14258c9a3d5c40
Nick <nick.nvr@yahoo.com>
2025-06-05 17:22:58 -0500

    Use of other Classes Method
    Added the functionality for this class to use other classes method "getGreeting"

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class App {
   public static void main(String[] args) {
-    // Your code goes here
+    HelloWorld helloWorld = new HelloWorld();
+    System.out.println(helloWorld.getGreeting());
   }
 }
--- app/src/main/java/org/example/HelloWorld.java
+++ app/src/main/java/org/example/HelloWorld.java
@@ -1,5 +1,7 @@
 package org.example;
 
 public class HelloWorld {
-  // Your code goes here
+    String getGreeting(){
+      return "Hello World!";
+    }
 }
